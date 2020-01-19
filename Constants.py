eb027aa5b51d219aafa793364b61465054a3dcad
zachary <syyun@snu.ac.kr>
2020-01-19 18:23:45 +0900

    Add types of examples to constants

--- Constants.py
+++ Constants.py
@@ -1,6 +1,19 @@
 import numpy as np
 
+from nptyping import Array
+
+# Constants to be used
 x = np.mat([[-6.0], [6.0]])
 A = np.mat([[3.0, 2.0], [2.0, 6.0]])
 b = np.mat([[2.0], [-8.0]])
 c = 0.0
+
+# Types to be used
+dim = 2
+type_x = Array[float, dim, 1]
+type_A: Array[float, dim, dim]
+type_b: Array[float, dim, 1]
+type_c: float
+
+if __name__ == "__main__":
+    pass
