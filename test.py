87c3ff38ba0a1b247e0ee8c063e45896d63eafee
zachary <syyun@snu.ac.kr>
2020-01-19 17:55:37 +0900

    Add constants to test

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
 from unittest import TestCase
 
-import numpy as np
-
+from Constants import *
 import quadratic
 
 
 class TestSuite(TestCase):
     def test_quadratic_function(self):
-        x = np.mat([[-6.0], [6.0]])
-        A = np.mat([[3.0, 2.0], [2.0, 6.0]])
-        b = np.mat([[2.0], [-8.0]])
-        c = 0.0
         quadratic.f(x, A, b, c)
