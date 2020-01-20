6b25c87fc721a7ead9b5c0455e11108a8ca39706
zachary <syyun@snu.ac.kr>
2020-01-20 11:55:43 +0900

    Add test for derivative

--- test.py
+++ test.py
@@ -7,3 +7,6 @@ import quadratic
 class TestSuite(TestCase):
     def test_quadratic_function(self):
         quadratic.f(x, A, b, c)
+
+    def test_quadratics_derivative(self):
+        quadratic.f_derivative(x, A, b)
